55a1d9cb0fbe25dd60226b5722a4c5e7162b2260
root <shadster09@gmail.com>
2016-01-14 19:54:30 +0200

    some cleanup

--- Etch-a-Sketch/js/application.js
+++ Etch-a-Sketch/js/application.js
@@ -1,12 +1,15 @@
 var grid = $(".grid");
+
 $(document).ready(function() {
   var w = grid.width(); //get the width which is set at a percentage
   grid.width(Math.ceil(w)); //Make grid width equal to an int instead of float from the percentage
+
   $(".reset").on("click", reset);
   $(".newGrid").on("click", createGrid);
+
   grid.on("mouseenter", ".square", function () {
     $(this).addClass("black");
-  });
+  });  
 });
 
 function reset() {
@@ -17,8 +20,10 @@ function createGrid() {
   var n = prompt("Please enter size of the new grid");
   var wh = grid.width() / n;
   grid.find("div").remove();
+
   for(var i = 0; i < n*n; i++) {
     grid.append("<div class='square'></div>");
   }
-  $(".square").width(wh).height(wh);
+
+  $(".square").width(wh).height(wh);  //adjusts width and height of the square
 }
